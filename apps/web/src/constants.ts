fed2ecc4f365949c748887aa2e48577c4b1b032d
William <wpf8741@pm.me>
2026-03-28 22:35:05 -0400

    chore: add repository metadata links

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -1,3 +1,2 @@
-// Update this once the repo is pushed to GitHub
-export const GITHUB_URL = "https://github.com/YOUR_USERNAME/get-steady";
+export const GITHUB_URL = "https://github.com/Aeltrion-Labs/get-steady";
 export const LICENSE_PATH = `${GITHUB_URL}/blob/main/LICENSE`;
